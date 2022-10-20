9082d8900c78d0e2e3a374151348e1c2c0091b1f
Justin <Tridell977@gmail.com>
2022-10-20 17:27:15 -0600

    test commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { fetchAPI} from "../api/api";
 
 
 const Home = ({posts, setPosts}) => {
-    return (<div id="Home"><h2></h2>
+    return (<div id="Home"><h2>Home</h2>
        {/* {posts ? <div className="allPosts">{posts.map((post, index) => <div className="singlePost" key={index}><h3>{post.title}</h3><p>{post.description}</p><p>Price: {post.price}</p></div>)}</div> : null} */}
         </div>
     )
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,7 +5,7 @@ const PostsTab = ({posts, setPosts}) => {
     useEffect(() => {fetchAPI("posts").then((result) => {
         setPosts(result.data.posts)
     })}, []);
-    return (<div id="Home"><h1>Home</h1>
+    return (<div id="Home"><h2>Posts</h2>
        {posts ? <div className="allPosts">{posts.map((post, index) => <div className="singlePost" key={index}><h3>{post.title}</h3><p>{post.description}</p><p>Price: {post.price}</p></div>)}</div> : null}
         </div>
     )
